422b52bd9f9aa96ede10514f51779334eb9444ee
Spencer Peterson <spencerthomasp@gmail.com>
2017-07-11 20:55:20 -0600

    Made as much progress with the Facebook API as possible. Cannot test posts from my webpage to FB until it is submitted for review. Moving onto another social media site

--- appPosts.js
+++ appPosts.js
@@ -28,6 +28,8 @@ function updateUserURL() {
     });
 }
 
+
+
 $(document).ready(function() {
     updateUserDescription();
     updateUserURL();
@@ -39,3 +41,38 @@ $(document).ready(function() {
 /*-----------------------------------------------------------------
 ------------        EXPERIMENTAL FACEBOOK TESTS         -----------
 -----------------------------------------------------------------*/
+
+// "js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.9";
+
+// REQUEST LAST NAME OF CURRENT USER
+/*
+function lastName() {
+    $('.post-to-fb').click(function() {
+        FB.api('/me', {fields: 'last_name'}, function(response) {
+            console.log(response);
+        })
+    })
+}
+*/
+
+// PUBLISH STATUS MESSAGE TO CURRENT USER'S FEED
+function publishFBPost() {
+    $('.post-to-fb').click(function() {
+
+        FB.login(function(response) {
+            console.log(response);
+        }, {scope: 'user_posts'});
+
+        var userPost = "This is a custom message sent from OUTSIDE Facebook!";
+
+        FB.api('/me/feed', 'post', { message: userPost }, function(response) {
+            if (!response || response.error) {
+                console.log('Error posting message');
+                console.log(response.error);
+            } else {
+                console.log('SUCCESS!');
+                console.log('Post ID: ' + response.id);
+            }
+        })
+    })
+}
